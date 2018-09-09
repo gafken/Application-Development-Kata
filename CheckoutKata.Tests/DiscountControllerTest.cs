323299b0ecf770cd7692e79162a7073bf715862c
Rob Gafken <rtd91@live.com>
2018-09-08 22:15:44 -0400

    Initial Discounts API Interface Setup

---
+++ CheckoutKata.Tests/DiscountControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using CheckoutKata.Controllers;
+using CheckoutKata.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CheckoutKata.Tests
+{
+    [TestClass]
+    public class DiscountControllerTest
+    {
+        private DiscountController controller { get; set; }
+
+        [ClassInitialize]
+        public void LoadCache()
+        {
+            CacheManager.InventoryCache.Add("UPC1", new InventoryItem("jerky", 1.25m));
+            CacheManager.InventoryCache.Add("UPC2", new InventoryItem("turkey", 1.5m));
+            CacheManager.InventoryCache.Add("UPC3", new InventoryItem("ham", 1.75m));
+            CacheManager.InventoryCache.Add("UPC4", new InventoryItem("pepper", 2m));
+            CacheManager.InventoryCache.Add("UPC5", new InventoryItem("pepperoni", 2.25m));
+            CacheManager.InventoryCache.Add("UPC6", new InventoryItem("ground beef", 2.5m));
+            CacheManager.InventoryCache.Add("UPC7", new InventoryItem("bacon", 2.75m));
+            CacheManager.InventoryCache.Add("UPC8", new InventoryItem("salami", 3m));
+            CacheManager.InventoryCache.Add("UPC9", new InventoryItem("veal", 3.25m));
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            controller = new DiscountController();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            controller = null;
+        }
+    }
+}
---
+++ CheckoutKata/Models/Markdown.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CheckoutKata.Models
+{
+    public class Markdown  //This should probably inherit from a base class but I want
+                           //To keep this simple for demonstration purposes
+    {
+        public string Identifier { get; set; }
+
+        public string MarkdownPrice { get; set; }
+    }
+}
